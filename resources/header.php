ad06b3cd373e98f9abbb3cc472945eca8da9d87b
cesargm1 <cg.gtav@gmail.com>
2025-02-21 00:47:06 +0100

    add icons

--- resources/header.php
+++ resources/header.php
@@ -1,16 +1,32 @@
 <header class="header">
     <nav>
         <ul>
-            <li><a href="/index.php">logo</a></li>
+            <li>
+                <div class="container__icon">
+                    <a href="/index.php"><img src="/svg/cart.svg" alt="home" class="icon"></a>
+                </div>
+            </li>
+
             <li>
                 <form action="/buscador.php" method="get">
-                    <label>
-                        <input name="buscador" type="search" placeholder="Busca tu pelicula">
-                    </label>
+                    <div class="container__serch">
+                        <label>
+                            <input class="search" name="buscador" type="search" placeholder="Busca tu pelicula">
+                        </label>
+
+                        <div class="container__icon">
+                            <a href="/buscador.php"><img src="/svg/glass.svg" alt="glass" class="icon"></a>
+                        </div>
+                    </div>
+
                 </form>
             </li>
 
-            <li><a href="/cesta.php">cesta</a></li>
+            <li class="cart">
+                <div class="container__icon">
+                    <a href="/cesta.php"><img src="/svg/cart.svg" alt="glass" class="icon"></a>
+                </div>
+            </li>
         </ul>
     </nav>
 </header>
