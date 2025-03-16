894eae9e9b24d4870a2ee49b5083d967e822d17f
cesar_css_love <cg.gtav@gmail.com>
2025-03-16 18:36:16 +0100

    swiper to local

--- public/index.php
+++ public/index.php
@@ -16,10 +16,10 @@ $peliculas = ObtenerPeliculas::getAll();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="svg/logo.svg" />
     <title>Document</title>
-    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script src="/js/swiper-bundle.min.js"></script>
     <link
         rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+        href="/css/swiper-bundle.min.css" />
     <link rel="stylesheet" href="/css/main.css">
     <link rel="stylesheet" href="/css/index.css">
 </head>
